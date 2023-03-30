2d5b35c704e90a6c36070eb9243e8647978a89f1
Vladimi <zapadpride@ukr.net>
2023-03-30 17:51:06 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Telegram\Bot\Api;
+
+$telegram = new Api('6268064568:AAG-9HsCU0av9zEveVTVo1O-wG9qzHFBVWI');
+
+$result = $telegram->getWebhookUpdates();
+
+$text = $result['message']['text'];
+$chat_id = $result['message']['chat']['id'];
+$name = $result['message']['from']['username'];
+$first_name = $result['message']['from']['first_name'];
+$last_name = $result['message']['from']['last_name'];
+
+if ($text == '/start') {
+    $reply = 'Hello' . $name;
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+}
+$mes = $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+var_dump($mes);
